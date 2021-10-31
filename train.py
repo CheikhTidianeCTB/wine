bd22ba2b290e0d1d6bbd2c4585ef06af8493ec12
CheikhTidianeCTB <41235187+CheikhTidianeCTB@users.noreply.github.com>
2021-10-31 15:38:17 +0100

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 # Set random seed
-seed = 42
+seed = 45
 
 ################################
 ########## DATA PREP ###########
@@ -23,7 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random
 #################################
 
 # Fit a model on the train section
-regr = RandomForestRegressor(max_depth=5, random_state=seed)
+regr = RandomForestRegressor(max_depth=7, random_state=seed)
 regr.fit(X_train, y_train)
 
 # Report training set score
